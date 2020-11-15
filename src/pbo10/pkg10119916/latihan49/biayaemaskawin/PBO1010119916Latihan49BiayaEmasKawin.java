5081eda4c3975d060833471b58b8195bc9d53f3e
DianArpian <>
2020-11-15 07:23:46 +0700

    Biaya Emas Kawin

---
+++ src/pbo10/pkg10119916/latihan49/biayaemaskawin/PBO1010119916Latihan49BiayaEmasKawin.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10.pkg10119916.latihan49.biayaemaskawin;
+
+import java.util.Scanner;
+/**
+ *
+ * @author 
+ * NAMA     : DIAN ARPIAN
+ * NIM      : 10119916
+ * KELAS    : IF10K
+ * Deskripsi Program : Biaya Emas 
+ */
+public class PBO1010119916Latihan49BiayaEmasKawin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Emas emas = new Emas();
+        Scanner scn1 = new Scanner(System.in);
+        Scanner scn2 = new Scanner(System.in);
+        
+        System.out.println("===Program Emas===");
+        System.out.print("Berat emas = ");
+        emas.setBeratEmas(scn1.nextFloat());
+        System.out.print("Harga emas per-gram : ");
+        emas.setHargaEmas(scn2.nextFloat());
+        System.out.println("");
+        System.out.println("Harga emas adalah = " + emas.totalBayar(15.7, 570000));
+    }
+    
+}
